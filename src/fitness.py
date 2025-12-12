1c28720799d4765e7b44f60dda77f0599337573d
urmyhxpe <134176459+urmyhxpe@users.noreply.github.com>
2025-12-12 19:44:48 +0000

    removed redundant elements from fitness.py

--- src/fitness.py
+++ src/fitness.py
@@ -14,22 +14,9 @@ class FitnessEvaluator:
         self.com_adaptive_coefficient = 1.0
 
     def evaluate(self, solution):
-        """
-        Evaluate a solution and compute fitness.
-
-        Returns:
-            (fitness, is_feasible, details_dict)
-        """
-        overlap_count = count_overlaps(solution)
-        oob_count = count_out_of_bounds(solution, self.container)
+        """ Evaluate fitness """
         total_weight = solution.total_weight()
         com_dev = com_deviation(solution, self.container)
-
-        # Penalty for geometry
-        geometry_penalty = (
-                overlap_count * self.overlap_penalty +
-                oob_count * self.bounds_penalty
-        )
 
         # Penalty for weight
         weight_excess = max(0, total_weight - self.container.max_weight)
@@ -42,7 +29,7 @@ class FitnessEvaluator:
                 (com_dev ** 2)
         )
 
-        total_penalty = geometry_penalty + weight_pen + com_penalty
+        total_penalty = weight_pen + com_penalty
 
         # Incentivise good use of space
         area_covered = sum(math.pi * p.cylinder.diameter / 2 ** 2 for p in solution.placed)
@@ -52,15 +39,11 @@ class FitnessEvaluator:
         fitness = total_penalty - (utilisation * 100)
 
         is_feasible = (
-                overlap_count == 0 and
-                oob_count == 0 and
                 weight_excess == 0 and
                 com_dev == 0
         )
 
         details = {
-            'overlap_count': overlap_count,
-            'oob_count': oob_count,
             'total_weight': total_weight,
             'weight_excess': weight_excess,
             'com_deviation': com_dev,
